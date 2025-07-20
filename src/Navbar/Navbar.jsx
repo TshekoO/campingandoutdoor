5b3fc6560b30bc16a66aae6abb2dca33a281352a
TshekoO <tshekoogotlhe101@gmail.com>
2025-07-20 15:08:08 +0200

    cart

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -109,7 +109,7 @@ const NavbarContainer = styled.nav`
 
     @media (max-width: 768px) {
         align-items: flex-start;
-        width: 93%;
+        width: 90%;
     }
 `;
 const Icon = styled.div`
