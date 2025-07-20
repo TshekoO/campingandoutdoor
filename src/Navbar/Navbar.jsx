d8673fce2ae521410d04a26ab85d3003c7c51210
TshekoO <tshekoogotlhe101@gmail.com>
2025-07-20 15:14:01 +0200

    nav

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -109,7 +109,7 @@ const NavbarContainer = styled.nav`
 
     @media (max-width: 768px) {
         align-items: flex-start;
-        width: 89%;
+        width: 90%;
     }
 `;
 const Icon = styled.div`
